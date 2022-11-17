0cdf76be02dfe322bad7bedebf60ee85f8696365
M-benjamin <mballabenjamin@yahoo.fr>
2022-11-17 01:22:04 +0100

    clean

--- khome-server/src/main.ts
+++ khome-server/src/main.ts
@@ -38,6 +38,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  const port = process.env.PORT || 5000;
+  await app.listen(port);
 }
 bootstrap();
